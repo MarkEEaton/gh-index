01a4642c840a9df51f44adf38b696c3861a1990c
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2016-04-23 22:06:12 -0400

    validate input

--- gh-index.py
+++ gh-index.py
@@ -1,8 +1,10 @@
 import json
+import re
 import urllib2
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+
 
 # load the html page
 @app.route('/')
@@ -19,13 +21,22 @@ def calculate():
     jsondata = []
     countlist = []
 
-    # take the input from the form and make the api call
+    # take the input and validate it
+    user = request.args.get("a")
+    if len(user) > 30:
+        return jsonify(result="[error : too many characters]")
+    elif re.compile(r'[^a-zA-Z0-9]').search(user):
+        return jsonify(result="[error : invalid characters \
+                               - use only A-Z and 0-9]")
+    else:
+        pass
+
+    # make the api call
     # the api breaks results into pages. iterate through pages
-    requestdata = request.args.get("a")
     for i in range(1, 3):
         data.append(urllib2.urlopen(
-                    "https://api.github.com/users/{}/repos?per_page=100&page={}"
-                    .format(requestdata, i)))
+            "https://api.github.com/users/{}/repos?per_page=100&page={}"
+            .format(user, i)))
 
     # turn the api call data into json
     for i in data:
