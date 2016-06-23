03386c78619ac78d599889f94a8ac66f7df5646a
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2016-06-23 21:57:14 +0000

    added 403, changed file names, changed text

--- ghindex.py
+++ ghindex.py
@@ -84,4 +84,4 @@ def calculate():
         return jsonify(result=str(max(countlist)))
 
 if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.1', debug=True)
+    app.run(port=8000, host='127.0.0.1')
